317af5d4d55fa4da1a86570d39c4742724b6f227
Suman Kumar <techelecto@gmail.com>
2025-05-21 17:02:15 +0530

    Optimization

--- Youtube_Downloader.py
+++ Youtube_Downloader.py
@@ -1,34 +1,23 @@
-#!/usr/bin/python3
-from pytube import YouTube
-from colorama import Fore
-import os, sys
+import yt_dlp
+import shutil
+import os
 
-def Youtube_download():
-    save_path = os.getcwd()
-    link = input("Enter the link: ")
-    try:
-        yt = YouTube(link)
-    except:
-        print(Fore.RED + "Connection Error! please verify if URL is correct." + Fore.RESET)
-        sys.exit()
-    else:
-        print(Fore.GREEN + "URL is OK" + Fore.RESET)
-    #Title of the video
-    print(Fore.CYAN + "Title:" + Fore.RESET, yt.title)
-    #Length of the video
-    print(Fore.CYAN + "Length:" + Fore.RESET, round(yt.length/60, 2), "mins")
-    # Views
-    print(Fore.CYAN + "Views:" + Fore.RESET, yt.views)
-    #Rating
-    print(Fore.CYAN + "Rating:" + Fore.RESET, yt.rating)
-    try:
-        print(Fore.YELLOW + "Downloading..." + Fore.RESET)
-        ys = yt.streams.get_highest_resolution()
-        ys.download(save_path)
-    except:
-        print(Fore.RED + "Something went wrong" + Fore.RESET)
-    else:
-        print(Fore.GREEN + "Download completed and saved to {}".format(save_path) + Fore.RESET)
+def yt_download():
+    ffmpeg_path = "D:\\Apps\\ffmpeg_2023\\bin"  # Update this to the directory containing ffmpeg binary
+    if not shutil.which("ffmpeg", path=ffmpeg_path):
+        print("ffmpeg is not installed in the specified directory. Please check the path.")
+        return
 
-if __name__ =="__main__":
-    Youtube_download()
+    os.environ["PATH"] += os.pathsep + ffmpeg_path
+
+    save_path = "C:\\Users\\Admin\\Downloads"
+    link = input("URL: ")
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': save_path + '/%(title)s.%(ext)s',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([link])
+    print("Success")
+
+yt_download()
